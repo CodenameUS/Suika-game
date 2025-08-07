139fe435a58a0765eb6f22e2747dee995d100164
Useong <104291987+CodenameUS@users.noreply.github.com>
2025-08-07 16:32:59 +0900

    Fruit 스크립트 생성

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
-    private void Update()
+    public bool isDrag;
+    private Rigidbody2D rigid;
+
+    private void Awake()
     {
-        GetInput();
+        rigid = GetComponent<Rigidbody2D>();
     }
 
-    private void GetInput()
+    private void Update()
     {
+        MoveFruit();
+    }
+
+    private void MoveFruit()
+    {
+        if (!isDrag) return;
+
+        // 마우스 포인터 따라가기
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // 가로로 최대 이동가능한 영역
+        float minX = -2.4f + transform.localScale.x / 2f;
+        float maxX = 2.4f - transform.localScale.x / 2f;
+        float clampedX = Mathf.Clamp(mousePos.x, minX, maxX);
+
+        // 가로로만 이동
         mousePos.z = 0;
-        transform.position = mousePos;
+        mousePos.y = transform.position.y;
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+    }
+    
+    // 드래그
+    
+    public void Drag()
+    {
+        isDrag = true;
+    }
+
+    // 과일 드랍
+    public void Drop()
+    {
+        isDrag = false;
+        rigid.simulated = true;
     }
 }
